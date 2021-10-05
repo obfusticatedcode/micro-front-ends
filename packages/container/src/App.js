e7fbaad0280057e71f7da40c821df4323d7b3868
Chisomo Lungu <chisomo.lungu@concentra.co.uk>
2021-10-05 11:51:21 +0100

    refactor: fix class name conflicts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
