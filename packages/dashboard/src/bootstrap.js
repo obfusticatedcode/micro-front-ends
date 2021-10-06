796c78ff92b9b4057f6cb190053763f95fd116b9
Chisomo Lungu <chisomo.lungu@concentra.co.uk>
2021-10-06 21:19:24 +0100

    feature: we now have a dashboard using vue

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
+
+// call mount to start the app
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// in local dev mode, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_dashboard-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
