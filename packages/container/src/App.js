46a665ef8deb2f556c870d22baee1d57c7e80f26
Chisomo Lungu <chisomo.lungu@concentra.co.uk>
2021-10-06 21:44:35 +0100

    deploy: dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,25 +1,35 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import { CircularProgress } from "@material-ui/core";
+import { createBrowserHistory } from "history";
 
 const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
 const AuthAppLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({ productionPrefix: "co" });
+
+const history = createBrowserHistory();
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
   const handleSignIn = () => setIsSignedIn(true);
   const handleSignOut = () => setIsSignedIn(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <>
           <Header isSignedIn={isSignedIn} onSignOut={handleSignOut} />
           <Suspense
@@ -42,11 +52,16 @@ const App = () => {
               <Route path="/auth">
                 <AuthAppLazy onSignIn={handleSignIn} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
+
               <Route path="/" component={MarketingAppLazy} />
             </Switch>
           </Suspense>
         </>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
